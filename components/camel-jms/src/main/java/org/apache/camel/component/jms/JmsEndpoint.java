rnetuka/camel-1

[ENTESB-15117] Implement consumer priority in camel

components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
@@ -183,6 +183,9 @@ public class JmsEndpoint extends DefaultEndpoint implements AsyncEndpoint, Heade
 
     public void configureListenerContainer(AbstractMessageListenerContainer listenerContainer, JmsConsumer consumer) {
         if (destinationName != null) {
+            if (getConfiguration().getConsumerPriority() != 0) {
+                destinationName += "?consumer-priority=" + getConfiguration().getConsumerPriority();
+            }
             listenerContainer.setDestinationName(destinationName);
             log.debug("Using destinationName: {} on listenerContainer: {}", destinationName, listenerContainer);
         } else if (destination != null) {

jboss-fuse/camel

components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
@@ -186,6 +186,9 @@ public class JmsEndpoint extends DefaultEndpoint implements AsyncEndpoint, Heade
 
     public void configureListenerContainer(AbstractMessageListenerContainer listenerContainer, JmsConsumer consumer) {
         if (destinationName != null) {
+            if (getConfiguration().getArtemisConsumerPriority() != 0) {
+                destinationName += "?consumer-priority=" + getConfiguration().getArtemisConsumerPriority();
+            }
             listenerContainer.setDestinationName(destinationName);
             log.debug("Using destinationName: {} on listenerContainer: {}", destinationName, listenerContainer);
         } else if (destination != null) {