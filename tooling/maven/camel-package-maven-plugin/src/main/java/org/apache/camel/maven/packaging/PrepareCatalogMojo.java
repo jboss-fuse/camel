d0d6c660ad62fa9e66675df3ee1e0bcbc73747a6
Peter Palaga <ppalaga@redhat.com>
2022-09-02 10:43:33 +0200

    Add skip option to prepare-catalog mojo

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -184,6 +184,12 @@ public class PrepareCatalogMojo extends AbstractMojo {
     protected File mainDir;
 
     /**
+     * Skip the execution of this mojo
+     */
+    @Parameter(defaultValue = "false", property = "camel.prepare-catalog.skip")
+    private boolean skip;
+
+    /**
      * Maven ProjectHelper.
      */
     @Component
