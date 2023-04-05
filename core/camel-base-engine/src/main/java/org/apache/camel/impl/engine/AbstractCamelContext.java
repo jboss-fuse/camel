c467d54af9dd470151a8d9fde4c7d7d4c8bcd7a0
Tom Cunningham <tcunning@redhat.com>
2023-04-05 08:18:50 -0400

    Revert "CAMEL-19026: camel-main - Add camel.main.backlogTracingStandby option."
    This reverts commit 79111b46434b1c7ad1b1d92ecd34b1478a40cf22.

--- core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
+++ core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
@@ -261,7 +261,6 @@ public abstract class AbstractCamelContext extends BaseService
     private Initialization initialization = Initialization.Default;
     private Boolean autoStartup = Boolean.TRUE;
     private Boolean backlogTrace = Boolean.FALSE;
-    private Boolean backlogTraceStandby = Boolean.FALSE;
     private Boolean trace = Boolean.FALSE;
     private Boolean traceStandby = Boolean.FALSE;
     private String tracePattern;
@@ -2324,14 +2323,6 @@ public abstract class AbstractCamelContext extends BaseService
     @Override
     public void setBacklogTracing(Boolean backlogTrace) {
         this.backlogTrace = backlogTrace;
-    }
-
-    public Boolean getBacklogTraceStandby() {
-        return backlogTraceStandby;
-    }
-
-    public void setBacklogTraceStandby(Boolean backlogTraceStandby) {
-        this.backlogTraceStandby = backlogTraceStandby;
     }
 
     @Override
@@ -4896,16 +4887,6 @@ public abstract class AbstractCamelContext extends BaseService
     @Override
     public boolean isTracingStandby() {
         return traceStandby != null && traceStandby;
-    }
-
-    @Override
-    public void setBacklogTracingStandby(boolean backlogTracingStandby) {
-        this.backlogTraceStandby = backlogTracingStandby;
-    }
-
-    @Override
-    public boolean isBacklogTracingStandby() {
-        return backlogTraceStandby != null && backlogTraceStandby;
     }
 
     @Override
--- core/camel-management/src/main/java/org/apache/camel/management/mbean/ManagedBacklogTracer.java
+++ core/camel-management/src/main/java/org/apache/camel/management/mbean/ManagedBacklogTracer.java
@@ -62,11 +62,6 @@ public class ManagedBacklogTracer implements ManagedBacklogTracerMBean {
     }
 
     @Override
-    public boolean isStandby() {
-        return backlogTracer.isStandby();
-    }
-
-    @Override
     public void setEnabled(boolean enabled) {
         backlogTracer.setEnabled(enabled);
     }
