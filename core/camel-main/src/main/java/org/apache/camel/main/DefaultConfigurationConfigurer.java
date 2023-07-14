a414a1b4895806a910b0f737b2bf3d199fecf942
Claus Ibsen <claus.ibsen@gmail.com>
2023-07-14 11:48:10 +0200

    CAMEL-19603: camel-core - configureVault should be done general so its also done for Spring Boot and Quarkus and not only camel-main/camel-jbang.

--- core/camel-main/src/main/java/org/apache/camel/main/DefaultConfigurationConfigurer.java
+++ core/camel-main/src/main/java/org/apache/camel/main/DefaultConfigurationConfigurer.java
@@ -570,6 +570,10 @@ public final class DefaultConfigurationConfigurer {
                 devConsoleRegistry.register(console);
             }
         }
+
+        // set the default thread pool profile if defined
+        initThreadPoolProfiles(registry, camelContext);
+
         // vaults
         AwsVaultConfiguration aws = getSingleBeanOfType(registry, AwsVaultConfiguration.class);
         if (aws != null) {
@@ -592,9 +596,6 @@ public final class DefaultConfigurationConfigurer {
             vault.setHashicorpVaultConfiguration(hashicorp);
         }
         configureVault(camelContext);
-
-        // set the default thread pool profile if defined
-        initThreadPoolProfiles(registry, camelContext);
     }
 
     /**
