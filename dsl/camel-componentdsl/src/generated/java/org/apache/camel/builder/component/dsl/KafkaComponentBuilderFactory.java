dbcbb8574e149a60228429d26beb1c9222445c11
Tom Cunningham <tcunning@redhat.com>
2023-09-27 13:12:18 -0400

    CAMEL-19921 - Update default values of kafka client configuration (#11579) (#2292)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    Co-authored-by: Andrea Cosentino <ancosen@gmail.com>

--- dsl/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/KafkaComponentBuilderFactory.java
+++ dsl/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/KafkaComponentBuilderFactory.java
@@ -335,7 +335,7 @@ public interface KafkaComponentBuilderFactory {
          * 
          * The option is a: &lt;code&gt;java.lang.Integer&lt;/code&gt; type.
          * 
-         * Default: 40000
+         * Default: 30000
          * Group: consumer
          * 
          * @param consumerRequestTimeoutMs the value to set
