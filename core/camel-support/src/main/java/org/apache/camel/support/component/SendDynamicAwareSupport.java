78f2222887092375db0a2859c67392d3ddfcb57d
Tom Cunningham <tcunning@redhat.com>
2023-10-30 16:48:21 -0400

    Revert "CAMEL-19663: camel-core - ToD EIP multi-value property lost problem. Thanks to EvanMi for the PR."
    This reverts commit 99b6bb8a7c9bc26aa717eedbcf90078353939b61.

--- core/camel-support/src/main/java/org/apache/camel/support/component/SendDynamicAwareSupport.java
+++ core/camel-support/src/main/java/org/apache/camel/support/component/SendDynamicAwareSupport.java
@@ -90,13 +90,7 @@ public abstract class SendDynamicAwareSupport extends ServiceSupport implements 
             // okay so only add the known properties as they are the non lenient properties
             properties = new LinkedHashMap<>();
             map.forEach((k, v) -> {
-                boolean accept = knownProperties.contains(k);
-                // we should put the key from a multi-value (prefix) in the
-                // properties too, or the property may be lost
-                if (!accept && !knownPrefixes.isEmpty()) {
-                    accept = knownPrefixes.stream().anyMatch(k::startsWith);
-                }
-                if (accept) {
+                if (knownProperties.contains(k)) {
                     properties.put(k, v);
                 }
             });
